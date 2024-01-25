b9d317c2df7f66d5e874aefec14ba02c43880eb1
HuangWei <market@zuiyue.com>
2024-01-25 14:04:53 +0800

    1.2.7

--- wei/windows/latest/data/dist/static/js/element-plus.3f7249cb.js
+++ wei/windows/latest/data/dist/static/js/element-plus.3f7249cb.js
@@ -1,7 +1,7 @@
 /*!
  *  build: Vue  Admin Plus
  *  copyright: vue-admin-beautiful.com
- *  time: 2024-01-25 10:24:11
+ *  time: 2024-01-25 13:58:32
  */
 "use strict";(self["webpackChunkadmin_plus"]=self["webpackChunkadmin_plus"]||[]).push([[2044],{43977:function(e,t,l){l.d(t,{Z:function(){return n}});
 /*! Element Plus v2.3.14 */
